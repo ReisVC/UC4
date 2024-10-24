b7009456571305421797f271c14fcdd7478dc2c8
cintiakuhn66 <vitor.reisvc@gmail.com>
2024-10-24 17:04:05 -0300

    Academia

---
+++ ACADEMIA/academia.js
@@ -0,0 +1,55 @@
+var Instrutor = /** @class */ (function () {
+    function Instrutor(nome, especialidade, dataNascimento) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.dataNascimento = dataNascimento;
+    }
+    return Instrutor;
+}());
+var Plano;
+(function (Plano) {
+    Plano[Plano["mensal"] = 0] = "mensal";
+    Plano[Plano["semestral"] = 1] = "semestral";
+    Plano[Plano["anual"] = 2] = "anual";
+})(Plano || (Plano = {}));
+var Treino = /** @class */ (function () {
+    function Treino(nome, tipo, serie, repeticao) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.serie = serie;
+        this.repeticao = repeticao;
+    }
+    return Treino;
+}());
+var Aluno = /** @class */ (function () {
+    function Aluno(nome, instrutor, plano, tipoTreino) {
+        this.treinos = [];
+        this.nome = nome;
+        this.instrutor = instrutor;
+        this.plano = plano;
+        this.tipoTreino = tipoTreino;
+    }
+    Aluno.prototype.cadastroTreino = function (treino) {
+        this.treinos.push(treino);
+    };
+    return Aluno;
+}());
+var Equipamento = /** @class */ (function () {
+    function Equipamento(nome, quantidade, dimensao, capacidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.dimensao = dimensao;
+        this.capacidade = capacidade;
+    }
+    return Equipamento;
+}());
+var instrutor1 = new Instrutor('Muzy', 'Musculação', new Date('1982-04-12'));
+var supino = new Treino('Supino', 'Peito', 4, 20);
+var rosca = new Treino('Rosca', 'Bíceps', 3, 12);
+var leg = new Treino('Leg Press', 'Pernas', 4, 15);
+var equipamento1 = new Equipamento('Flexora', 10, '120x150', 150);
+var equipamento2 = new Equipamento('Esteira', 30, '150x130', 200);
+var equipamento3 = new Equipamento('Bicicleta', 10, '120x120', 130);
+var aluno1 = new Aluno('Vitor', instrutor1, Plano.anual, 'Musculação');
+aluno1.cadastroTreino(supino);
+aluno1.cadastroTreino(rosca);
---
+++ ACADEMIA/academia.ts
@@ -0,0 +1,85 @@
+class Instrutor 
+{
+    nome: string
+    especialidade: string
+    dataNascimento: Date
+
+    constructor(nome: string, especialidade: string, dataNascimento: Date) {
+        this.nome = nome
+        this.especialidade = especialidade
+        this.dataNascimento = dataNascimento
+    }
+}
+
+enum Plano 
+{
+    mensal,
+    semestral,
+    anual
+}
+
+class Treino
+{
+    nome: string
+    tipo: string
+    serie: number
+    repeticao: number
+
+    constructor(nome: string, tipo: string, serie: number, repeticao: number) {
+        this.nome = nome
+        this.tipo = tipo
+        this.serie = serie
+        this.repeticao = repeticao
+    }
+}
+
+class Aluno 
+{
+    nome: string
+    instrutor: Instrutor
+    plano: Plano
+    tipoTreino: string
+    treinos: Array<Treino> = []
+
+    constructor(nome: string, instrutor: Instrutor, plano: Plano, tipoTreino: string) {
+    this.nome = nome
+    this.instrutor = instrutor
+    this.plano = plano
+    this.tipoTreino = tipoTreino
+    }
+
+    cadastroTreino(treino: Treino) {
+        this.treinos.push(treino)
+    }
+    
+}
+
+class Equipamento
+{
+    nome: string
+    quantidade: number
+    dimensao: string
+    capacidade: number
+
+    constructor(nome: string, quantidade: number, dimensao: string, capacidade: number) {
+        this.nome = nome
+        this.quantidade = quantidade
+        this.dimensao = dimensao
+        this. capacidade = capacidade
+    }
+}
+
+let instrutor1: Instrutor = new Instrutor('Muzy', 'Musculação', new Date('1982-04-12'))
+
+let supino: Treino = new Treino('Supino', 'Peito', 4, 20)
+let rosca: Treino = new Treino('Rosca', 'Bíceps', 3, 12)
+let leg: Treino = new Treino('Leg Press', 'Pernas', 4, 15)
+
+let equipamento1: Equipamento = new Equipamento('Flexora', 10, '120x150', 150)
+let equipamento2: Equipamento = new Equipamento('Esteira', 30, '150x130', 200)
+let equipamento3: Equipamento = new Equipamento('Bicicleta', 10, '120x120', 130)
+
+let aluno1: Aluno = new Aluno('Vitor', instrutor1, Plano.anual, 'Musculação')
+aluno1.cadastroTreino(supino)
+aluno1.cadastroTreino(rosca)
+
